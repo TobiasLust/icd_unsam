eee302676e09187def799dd32feba8a120216080
unknown <tobiaslust.dev@gmail.com>
2025-03-12 21:34:32 -0300

    clase_2

--- clase_1/lifecycle/lifecycle.R
+++ clase_1/lifecycle/lifecycle.R
@@ -1,2 +1,6 @@
-ggplot(data,mapping = aes(x=costo_agua,y=expectativa_vida, color=factor(continente))) +
-  geom_point()
+library(tidyverse)
+
+ggplot(data,mapping = aes(x=promedio_trabajo_anual,y=nivel_felicidad, color=factor(continente), size= rank)) +
+  geom_point() +
+  ylim(5,8) +
+  scale_color_discrete(name='Continente', labels=c('Africa','America','Asia','Europa','Oceania'))
